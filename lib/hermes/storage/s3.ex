b81076e3b2448eb41b4d919f1d62cafa7e717b1a
Vicente Sixto Cárdenas <v.cardenas@muppy.com>
2026-05-08 14:04:50 +0200

    Drop public_url logging to avoid presigned URL leak

--- lib/hermes/storage/s3.ex
+++ lib/hermes/storage/s3.ex
@@ -42,14 +42,10 @@ defmodule Hermes.Storage.S3 do
     bucket = config(:bucket)
     region = config(:region)
 
-    Logger.info("S3.public_url start bucket=#{bucket} region=#{region} key=#{key}")
-
     {:ok, url} =
       ExAws.S3.presigned_url(ExAws.Config.new(:s3, region: region), :get, bucket, key,
         expires_in: @signed_url_expires
       )
-
-    Logger.info("S3.public_url result key=#{key} status=ok")
 
     url
   end
