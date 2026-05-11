a993f65141a0ab805543320efb15703d4b2d2199
Vicente Cárdenas <157356244+vsixto-cardenas@users.noreply.github.com>
2026-05-11 09:38:39 +0200

    Log S3 storage operations (#35)
    * Remove undeclared aws_s3_bucket and aws_s3_host vars from Terraform plan
    
    * Log S3 upload, delete, and presigned URL operations
    
    * Avoid logging presigned URL to prevent credential leak
    
    * Drop public_url logging to avoid presigned URL leak
    
    * Escape user-supplied content_type in upload log to prevent log injection

--- lib/hermes/storage/s3.ex
+++ lib/hermes/storage/s3.ex
@@ -3,20 +3,39 @@ defmodule Hermes.Storage.S3 do
   AWS S3 storage adapter.
   """
 
+  require Logger
+
   @signed_url_expires 3600
 
   def upload(key, binary, content_type) do
     bucket = config(:bucket)
+    size = byte_size(binary)
 
-    ExAws.S3.put_object(bucket, key, binary, content_type: content_type)
-    |> ExAws.request(ex_aws_opts())
+    Logger.info(
+      "S3.upload start bucket=#{bucket} key=#{key} content_type=#{inspect(content_type)} size=#{size}"
+    )
+
+    result =
+      ExAws.S3.put_object(bucket, key, binary, content_type: content_type)
+      |> ExAws.request(ex_aws_opts())
+
+    Logger.info("S3.upload result key=#{key} result=#{inspect(result)}")
+
+    result
   end
 
   def delete(key) do
     bucket = config(:bucket)
 
-    ExAws.S3.delete_object(bucket, key)
-    |> ExAws.request(ex_aws_opts())
+    Logger.info("S3.delete start bucket=#{bucket} key=#{key}")
+
+    result =
+      ExAws.S3.delete_object(bucket, key)
+      |> ExAws.request(ex_aws_opts())
+
+    Logger.info("S3.delete result key=#{key} result=#{inspect(result)}")
+
+    result
   end
 
   def public_url(key) do
